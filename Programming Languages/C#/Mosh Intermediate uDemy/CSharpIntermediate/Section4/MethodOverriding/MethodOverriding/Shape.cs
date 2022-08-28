0eda6e6c3816cf57d2ddc7b5313583cbb501d64c
Nathanael Cammay <nathanaelcammay@gmail.com>
2022-08-28 12:26:09 +0200

    update

--- Programming Languages/C#/Mosh Intermediate uDemy/CSharpIntermediate/Section4/MethodOverriding/MethodOverriding/Shape.cs
+++ Programming Languages/C#/Mosh Intermediate uDemy/CSharpIntermediate/Section4/MethodOverriding/MethodOverriding/Shape.cs
@@ -1,30 +1,5 @@
-﻿using System;
-
-namespace MethodOverriding
+﻿namespace MethodOverriding
 {
-    public class Circle : Shape
-    {
-        public override void Draw()
-        {
-            Console.WriteLine("Draw a circle");
-        }
-    }
-
-    public class Rectangle : Shape
-    {
-        public override void Draw()
-        {
-            Console.WriteLine("Draw a rectangle");
-        }
-    }
-
-    public class Triangle : Shape
-    {
-        public override void Draw()
-        {
-            Console.WriteLine("Draw a triangle");
-        }
-    }
 
     public class Shape
     {
---
+++ Programming Languages/C#/Mosh MVC/Vidly/Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // creating a custom route
+
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    // adding constraints to the URL entry    
+            //    new {year = @"2015|2016", month = @"\d{2}" });
+
+            //creating attribute routes (better)
+
+            routes.MapMvcAttributeRoutes();
+
+            // this is the default route
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
