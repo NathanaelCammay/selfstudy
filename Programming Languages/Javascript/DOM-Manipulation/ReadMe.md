bd53252d0b98d698d89d949e1f0e0383610f8dc7
Nathanael Cammay <nathanaelcammay@gmail.com>
2022-09-12 20:14:09 +0200

    DOM-manipulation vanilla JavaScript

---
+++ Programming Languages/Javascript/DOM-Manipulation/ReadMe.md
@@ -0,0 +1,15 @@
+# Document Object Model Manipulation
+
+## This is the means by which we manipulate our document object model elements using JavaScript.
+
+### Tutorial video link:
+
+https://www.youtube.com/watch?v=y17RuWkWdn8
+
+(Big thanks to Web Dev Simplified)
+
+#### Notes:
+
+- The "document" class is the parent class that will give you access to methods that involve DOM-Manipulation
+- querySelector method allows you to pass in element you're trying to select
+- getAttribute method allows you to get the attribute
---
+++ Programming Languages/Javascript/DOM-Manipulation/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//////////////// 1. add elements to page
+
+// append allows you to append strings
+//appendChild only allows you to append elements
+
+// const body = document.body;
+// body.append("Hello World");
+
+// const div = document.createElement("div");
+
+///////////////// 2. adding text to element
+// div.innerText = "Hello World"
+// div.textContent = "Hello World 2";
+// div.innerHTML = "<strong>Hello World 2</strong>";
+// body.append(div);
+
+////////////////// 3. Removing an element
+
+// const body = document.body;
+// const div = document.querySelector("div");
+// const spanHi = document.querySelector("#hi");
+// const spanBye = document.querySelector("#bye");
+
+// //removes Bye element from entire html page
+// spanBye.remove()
+// // add it back
+// div.append(spanBye)
+
+/////////////////// 4. Adding elements
+
+//modifying elements
+
+const spanHi = document.querySelector("#hi");
+spanHi.getAttribute("íd")
